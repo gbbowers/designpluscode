798aa09eef05e8110a66ba72e6a45fc323c74cd7
gbbowers <gbbowers@gmail.com>
2018-07-30 18:55:16 -0500

    fixed the bug by restarting. durp.

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,16 +2,52 @@ import React from 'react'
 import styled from 'styled-components'
 
 const SectionGroup = styled.div`
-    background: black;
-    height: 720px;
+    margin: 100px 0 0;
+	background: url(${props => props.image});
+	background-size: cover;
+	height: 720px;
+	display: grid;
+	grid-template-rows: 300px auto;
+	grid-gap: 20px;
+	position: relative;
+	
+	@media (max-width: 640px) {
+		height: 820px;
+	}
 `
-const SectionLogo = styled.img``
+const SectionLogo = styled.img`
+    width: 128px;
+	margin: 0 auto;
+	align-self: end;
+`
 
-const SectionTitleGroup = styled.div``
+const SectionTitleGroup = styled.div`
+    max-width: 800px;
+	margin: 0 40px;
+	display: grid;
+	grid-gap: 20px;
+	grid-template-columns: 300px auto;
+	grid-template-rows: auto 100%;
 
-const SectionTitle = styled.h3``
+	@media (max-width: 720px) {
+		grid-template-columns: 1fr;
+	}
+`
 
-const SectionText = styled.p``
+const SectionTitle = styled.h3`
+    color: white;
+	font-size: 60px;
+	margin: 0;
+	line-height: 1.2;
+	
+	@media (max-width: 640px) {
+		font-size: 40px;
+	}
+`
+
+const SectionText = styled.p`
+    color: white;
+`
 
 
 const Section = props => (
